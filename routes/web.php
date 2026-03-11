12519b79379eb1f4cacf2cd8745e78d1c907fc2f
LGoat <issimo1122@gmail.com>
2026-03-11 03:08:22 +0000

    Update Our Routes And Add Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use App\Models\Message;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
@@ -8,41 +9,55 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route Get Test:
+// Using Our Routes Without Controllers
+// Route::method('/route', function(){});
 
+// // Create A GET Route Contact
+// // Access To Contact View With A GET Method
 // Route::get('/contact', function() {
-//     return '<p> Hello </p>';
+//     return view('contact');
 // });
+
+// // Create A POST Route Contact
+// // Handle And Validate Contact Infos WIth A POST Method
+// Route::post('/contact', function(Request $req){
+//         $validated = $req->validate([
+//         'name' => 'required',
+//         'email' => 'required|email',
+//         'message' => 'required|min:8|max:1000'
+//     ]);
+
+//     // Use Our Message Model
+//     Message::create([
+//         'sender_name' => $validated['name'],
+//         'sender_email' => $validated['email'],
+//         'message' => $validated['message'],
+
+//     ]);
+
+//     return redirect('/messages');
+// });
+
+// // Create A GET Route Messages
+// Route::get('/messages', function(){
+
+//     // Pass Our Messages To The Messages View
+//     return view('messages', ['messages' => Message::all()]);
+// });
+
+
+// Using Our Routes With Controllers
+// Route::method('/route', [NameController::class, 'Controller Function Name']);
+
+
 
 // Create A GET Route Contact
 // Access To Contact View With A GET Method
-Route::get('/contact', function() {
-    return view('contact');
-});
+Route::get('/contact', [ContactController::class, 'showForm']);
 
 // Create A POST Route Contact
 // Handle And Validate Contact Infos WIth A POST Method
-Route::post('/contact', function(Request $req){
-    $validated = $req->validate([
-        'name' => 'required',
-        'email' => 'required|email',
-        'message' => 'required|min:8|max:1000'
-    ]);
-
-    // Use Our Message Model
-    Message::create([
-        'sender_name' => $validated['name'],
-        'sender_email' => $validated['email'],
-        'message' => $validated['message'],
-
-    ]);
-
-    return redirect('/messages');
-});
+Route::post('/contact', [ContactController::class, 'storeMessage']);
 
 // Create A GET Route Messages
-Route::get('/messages', function(){
-
-    // Pass Our Messages To The Messages View
-    return view('messages', ['messages' => Message::all()]);
-});
+Route::get('/messages', [ContactController::class, 'showMessages']);
