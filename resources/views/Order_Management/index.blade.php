6bf777b13be4cae2c7f1f69e35e48a2a08b30fa8
LGoat <issimo1122@gmail.com>
2026-03-11 17:32:20 +0000

    Feat: Index For Access To Clients And Products Views

---
+++ resources/views/Order_Management/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Gestion des Commandes</h1>
+    <ul>
+        <li><a href="{{ route('client.index') }}">Gerer les clients</a></li>
+        <li><a href="{{ route('produit.index') }}">Gerer les Produits</a></li>
+    </ul>
+</body>
+
+</html>
